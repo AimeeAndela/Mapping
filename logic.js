18d889eb9ad91566e35dedf6044f2cbcb3356627
AimeeAndela <55422122+AimeeAndela@users.noreply.github.com>
2020-04-08 12:35:32 -0700

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,50 @@
+var myMap = L.map("map", {
+  center: [37.09, -95.71],
+  zoom: 5
+});
+
+// Add a tile layer
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
+
+d3.json(url, function(response) {
+
+  createFeatures(response.features);
+  console.log(response);
+
+  function createFeatures(earthquakeData) {
+
+    // Define a function we want to run once for each feature in the features array
+    // Give each feature a popup describing the place and time of the earthquake
+    function onEachFeature(feature, layer) {
+      layer.bindPopup("<h3>" + feature.properties.place +
+        "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+    }
+  
+    // Create a GeoJSON layer containing the features array on the earthquakeData object
+    // // Run the onEachFeature function once for each piece of data in the array
+    var earthquakes = L.geoJSON(earthquakeData, {
+      onEachFeature: onEachFeature
+    });
+
+    function markerSize(mag) {
+      return mag / 40;
+    }
+
+// Loop through the cities array and create one marker for each city object
+for (var i = 0; i < feature.properties.length; i++) {
+  L.circle(feature.properties[i].location, {
+    fillOpacity: 0.75,
+    color: "white",
+    fillColor: "purple",
+    // Setting our circle's radius equal to the output of our markerSize function
+    // This will make our marker's size proportionate to its population
+    radius: markerSize(feature.properties[i].mag)
+  }).bindPopup("<h1>" + feature.properties[i].place + "</h1> <hr> <h3>Magnitude: " + feature.properties[i].mag + "</h3>").addTo(myMap);
+};
